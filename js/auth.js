f688e88e92069e6b58235b5a156396de39406415
FlashMaster-cyber <asblockmangovn@gmail.com>
2026-08-03 21:39:53 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,54 @@
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
+const auth = window.firebaseAuth;
+
+// Đăng ký
+window.register = async function(email, password){
+
+    try{
+
+        await createUserWithEmailAndPassword(auth, email, password);
+
+        alert("Đăng ký thành công!");
+
+    }
+
+    catch(error){
+
+        alert(error.message);
+
+    }
+
+};
+
+// Đăng nhập
+window.login = async function(email, password){
+
+    try{
+
+        await signInWithEmailAndPassword(auth, email, password);
+
+        alert("Đăng nhập thành công!");
+
+    }
+
+    catch(error){
+
+        alert(error.message);
+
+    }
+
+};
+
+// Đăng xuất
+window.logout = async function(){
+
+    await signOut(auth);
+
+    alert("Đã đăng xuất");
+
+};
