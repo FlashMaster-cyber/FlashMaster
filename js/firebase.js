cbf61f45fca3e3b1507ee547782414ebcdf19f13
FlashMaster-cyber <asblockmangovn@gmail.com>
2026-08-03 21:24:21 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,40 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+
+// Firebase Config
+const firebaseConfig = {
+
+    apiKey: "API_KEY",
+
+    authDomain: "AUTH_DOMAIN",
+
+    projectId: "PROJECT_ID",
+
+    storageBucket: "STORAGE_BUCKET",
+
+    messagingSenderId: "SENDER_ID",
+
+    appId: "APP_ID"
+
+};
+
+
+// Initialize
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+
+// Export
+window.firebaseAuth = auth;
+
+window.firebaseDB = db;
+
+console.log("Firebase Connected!");
